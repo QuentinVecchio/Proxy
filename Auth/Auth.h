52eb7b3ef8d2c41aee36072ce43e565e4a868834
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-11-10 17:57:53 +0100

    suite avec thread, pbleme avec liste

--- Auth/Auth.h
+++ Auth/Auth.h
@@ -2,11 +2,28 @@
 #define AUTH_H
 
 #include "../ListeGenerique/listeGenerique.h"
-
+#include <pthread.h>
 typedef struct{
 	char* listeBlanche;
 	char* listeNoire;
 }Auth_Conf;
+
+
+#define NB_THREAD 2
+typedef struct{
+	char* lien;
+	int estDansListe[NB_THREAD];
+	Liste listeRecherche[NB_THREAD];
+	void* fonctionCmp[NB_THREAD];
+
+	int index;
+	pthread_mutex_t m_index;
+}Auth_Search;
+
+typedef struct{
+	char *lien;
+}Auth_Lien;
+
 
 Auth_Conf Auth_Var_Conf;
 
@@ -20,5 +37,8 @@ void init(Auth_Conf conf);
 void load();
 
 void loadListe(char* lien, Liste* l);
+
+int search(char* lien);
+
 
 #endif
