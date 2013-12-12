40844f13f96138845a32b3164e885c34c3ce547b
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-12-12 15:41:33 +0100

    debut keepElt, core dump >_<

--- ListeGenerique/listeGenerique.c
+++ ListeGenerique/listeGenerique.c
@@ -54,4 +54,27 @@ void* getElt(Liste* l, void* elt, int(*fonc)()){
 	return NULL;
 }
 
+void keepElt(Liste* l, int(*fonc)()){
+	if((*l) !=NULL){
+		if(fonc((*l)->courant)){
+			Element* elt = (*l);
+			(*l) = (*l)->suivant;
+			free(elt);
+		}else{
+			Liste* tmp = &(*l)->suivant;
+			Liste* avant = l;
+			do{
+				printf("oui\n");
+				if(fonc((*tmp)->courant)){
+					printf("Efface\n");
+					Element* elt = (*tmp);
+					(*avant)->suivant =(*tmp)->suivant;
+					free(elt);
+				}
+				avant = &(*avant)->suivant;
+				tmp = &(*tmp)->suivant;
+			}while((*tmp)->suivant);
+		}
+	}
+}
 
